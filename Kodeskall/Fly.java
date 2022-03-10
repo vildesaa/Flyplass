97b118a21884200d011c12dd2e8554fc3cb74fa2
vildesaa <101137017+vildesaa@users.noreply.github.com>
2022-03-10 17:52:07 +0100

    Update Fly.java

--- Kodeskall/Fly.java
+++ Kodeskall/Fly.java
@@ -1,9 +1,43 @@
 public class Fly implements Comparable<Fly> {
 
+    private String flyNummer;
+    private String til;
+    private String fra;
+    private int avgangstid;
+    private Fly foran = null;
+    private Fly etter = null;
+    
+    public Fly(String flyNr, String til, String fra, int avgangstid){
+        flyNummer = flyNr;
+        this.til = til;
+        this.fra = fra;
+        this.avgangstid = avgangstid;
+    }
+    public Fly hentForan(){
+        return foran;   
+    }
+    
+    public Fly hentEtter(){
+        return etter;        
+    }    
+     
     @Override
-    public String toString() {return null;}
+    public String toString() {return null;}{
+        String string = "Flynummer: " + flyNummer + "\nAvgangstid: " + avgangstid + "\nFra: " + fra + "\nTil: " + til;
+        return string;
+        
+    }    
 
     // Sorterer flyene fra foerste til siste avgangstid
     @Override
-    public int compareTo(Fly annen) {return 0;}
+    public int compareTo(Fly annen){
+        if(this.avgangstid < annen.avgangstid){
+            return -1;
+        }
+        else if (this.avgangstid > annen.avgangstid){
+            return 1;
+        } else {
+            return 0;
+        }    
+    }    
 }
